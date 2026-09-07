77fab604abc45502e81e85eb64f2e723ba4ef352
gcg5ktvhvr-sys <gcg5ktvhvr@privaterelay.appleid.com>
2026-09-08 01:39:04 +0800

    App entry point

---
+++ app.js
@@ -0,0 +1,14 @@
+App({
+  onLaunch: function () {
+    console.log('应用启动')
+  },
+  onShow: function () {
+    console.log('应用展示')
+  },
+  onHide: function () {
+    console.log('应用隐藏')
+  },
+  globalData: {
+    userInfo: null
+  }
+})
